48ab6b01e735a808dc26c62c7eaead01673bf37b
DheerajGogoi <dheerajgogoi2@gmail.com>
2021-05-22 21:10:28 +0530

    first commit

---
+++ a.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+
+public class a {
+	public static void main(String[] args) {
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		System.out.println(t);
+	}
+}
+
+
